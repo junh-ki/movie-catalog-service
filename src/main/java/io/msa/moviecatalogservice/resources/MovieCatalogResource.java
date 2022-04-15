f43ad247b678343d160f5a601bc08b3924712eb8
Junhyung Ki <kijoonh91@gmail.com>
2022-04-15 20:22:08 +0200

    Modify getCatalog to use /movies/{movieId} API from movie-info-service

--- src/main/java/io/msa/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/io/msa/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,12 +1,18 @@
 package io.msa.moviecatalogservice.resources;
 
 import io.msa.moviecatalogservice.models.CatalogItem;
+import io.msa.moviecatalogservice.models.Movie;
+import io.msa.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -14,7 +20,14 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(new CatalogItem("Transformers", "Test", 4));
+        RestTemplate restTemplate = new RestTemplate();
+        List<Rating> ratings = Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
+        return ratings.stream().map(rating -> Optional.ofNullable(restTemplate.getForObject("http://localhost:8082/movies/"
+                                + rating.getMovieId(), Movie.class))
+                        .map(movie -> new CatalogItem(movie.getName(), "Description", rating.getRating()))
+                        .orElse(null))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
 }
