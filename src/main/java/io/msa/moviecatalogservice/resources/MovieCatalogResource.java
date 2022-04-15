e6fe77e259860a3985e35a1ac7b2be1718988956
Junhyung Ki <kijoonh91@gmail.com>
2022-04-15 21:16:55 +0200

    Make restTemplate singleton with @Bean and @Autowired annotations

--- src/main/java/io/msa/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/io/msa/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package io.msa.moviecatalogservice.resources;
 import io.msa.moviecatalogservice.models.CatalogItem;
 import io.msa.moviecatalogservice.models.Movie;
 import io.msa.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,15 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        RestTemplate restTemplate = new RestTemplate();
         List<Rating> ratings = Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
         return ratings.stream().map(rating -> Optional.ofNullable(restTemplate.getForObject("http://localhost:8082/movies/"
                                 + rating.getMovieId(), Movie.class))
