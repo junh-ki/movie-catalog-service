5b4b69c372be5cd0932e8f2ee7c85ba7f56220e0
Junhyung Ki <kijoonh91@gmail.com>
2022-04-16 08:58:38 +0200

    Use the asynchronous webClient instead of the synchronous restTemplate

--- src/main/java/io/msa/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/io/msa/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,18 +20,29 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+    private final WebClient.Builder webClientBuilder;
 
     @Autowired
-    public void setRestTemplate(RestTemplate restTemplate) {
+    public MovieCatalogResource(RestTemplate restTemplate, WebClient.Builder webClientBuilder) {
         this.restTemplate = restTemplate;
+        this.webClientBuilder = webClientBuilder;
     }
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         List<Rating> ratings = Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
-        return ratings.stream().map(rating -> Optional.ofNullable(restTemplate.getForObject("http://localhost:8082/movies/"
+        /*return ratings.stream().map(rating -> Optional.ofNullable(restTemplate.getForObject("http://localhost:8082/movies/"
                                 + rating.getMovieId(), Movie.class))
+                        .map(movie -> new CatalogItem(movie.getName(), "Description", rating.getRating()))
+                        .orElse(null))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());*/
+        return ratings.stream().map(rating -> Optional.ofNullable(webClientBuilder.build().get()
+                                .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                                .retrieve()
+                                .bodyToMono(Movie.class)
+                                .block())
                         .map(movie -> new CatalogItem(movie.getName(), "Description", rating.getRating()))
                         .orElse(null))
                 .filter(Objects::nonNull)
